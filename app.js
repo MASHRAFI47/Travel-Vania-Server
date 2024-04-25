aeff894a20cf7a4f8bfe8e87794a3d4287e4d08b
MASHRAFI47 <mbngms@gmail.com>
2024-04-25 19:59:58 +0600

    cors and form json type added

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
 
+//cors
+const cors = require('cors');
+app.use(cors())
+
+//form urlencoded
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
 module.exports = app;
