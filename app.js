26130d9c62f9ca81e6420b13e6ab396038b4058f
MASHRAFI47 <mbngms@gmail.com>
2024-04-26 22:46:24 +0600

    find one tourist spot added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iduz7rm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,6 +35,13 @@ async function run() {
         app.get('/tourists', async(req, res) => {
             const cursor = touristSpotsCollection.find();
             const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/tourists/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await touristSpotsCollection.findOne(query);
             res.send(result)
         })
 
